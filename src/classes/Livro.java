49bf227e0547bfa4e04cf8c081ac85bf8e2efc3b
João Victor Superbi <57453381+joaosuperb1@users.noreply.github.com>
2023-05-07 16:17:53 -0300

    Update Livro.java

--- src/classes/Livro.java
+++ src/classes/Livro.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package classes;
 
 import java.util.ArrayList;
@@ -25,10 +21,6 @@ public class Livro {
         this.preco = 0.0;
         this.autores = new ArrayList<>();
         
-    }
-    
-    public Livro() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     public String getNomeLivro() {
